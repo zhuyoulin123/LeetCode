712ac27485d762cba556fcbf8b9bfc84e0cdba99
zhudabao <2414138198@qq.com>
2021-11-16 21:31:03 +0800

    N-20211116 21:30-新增有效字母异位词排序解法

---
+++ PrimaryAlgorithm/src/com/zyl/demo/string/IsAnagram.java
@@ -0,0 +1,18 @@
+package com.zyl.demo.string;
+
+/**
+ * @author zhuyoulin
+ * @program LeetCode
+ * @description 有效的字母异位词
+ * @create 2021-11-16 21:26
+ **/
+public class IsAnagram {
+  public boolean isAnagram(String s, String t) {
+    /**
+     * 给定两个字符串 s 和 t ，编写一个函数来判断 t 是否是 s 的字母异位词。
+     * 注意：若 s 和 t 中每个字符出现的次数都相同，则称 s 和 t 互为字母异位词。
+     * 先排序，排序之后相同的值会挨在一起，再比较数组是否一致即可
+     */
+
+  }
+}
