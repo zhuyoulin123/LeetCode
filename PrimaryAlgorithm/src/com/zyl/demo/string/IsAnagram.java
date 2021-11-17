028f420de81a685a8a8de5a676ac22eefb7ae183
zhudabao <2414138198@qq.com>
2021-11-17 21:38:08 +0800

    N-20211117 21:37-新增有效的字母异位词 Java API解法

--- PrimaryAlgorithm/src/com/zyl/demo/string/IsAnagram.java
+++ PrimaryAlgorithm/src/com/zyl/demo/string/IsAnagram.java
@@ -1,5 +1,7 @@
 package com.zyl.demo.string;
 
+import java.util.Arrays;
+
 /**
  * @author zhuyoulin
  * @program LeetCode
@@ -13,6 +15,11 @@ public class IsAnagram {
      * 注意：若 s 和 t 中每个字符出现的次数都相同，则称 s 和 t 互为字母异位词。
      * 先排序，排序之后相同的值会挨在一起，再比较数组是否一致即可
      */
-
+    char[] sChar = s.toCharArray();
+    char[] tChar = t.toCharArray();
+    //对两个字符串中的字符进行排序
+    Arrays.sort(sChar);
+    Arrays.sort(tChar);
+    return Arrays.equals(sChar, tChar);
   }
 }
