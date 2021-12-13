a28490ecd772f667b9e69ea78129064d9c4034c8
zhuyoulin <2414138198@qq.com>
2021-12-13 23:34:27 +0800

    N-20211213 23:34-新增验证回文串双指针解法以及正则表达式方法相关代码

--- PrimaryAlgorithm/src/com/zyl/demo/string/IsPalindrome.java
+++ PrimaryAlgorithm/src/com/zyl/demo/string/IsPalindrome.java
@@ -1,5 +1,7 @@
 package com.zyl.demo.string;
 
+import java.util.Locale;
+
 /**
  * @author zhuyoulin
  * @program LeetCode
@@ -32,6 +34,50 @@ public class IsPalindrome {
       if (s.charAt(i) != s.charAt(j)) {
         return false;
       }
+    }
+    return true;
+  }
+
+  /**
+   * 正则表达式解法
+   *
+   */
+  public static boolean isPalindrome2(String s) {
+    if (s == null || "".equals(s) || s.length() == 0) {
+      return true;
+    }
+    // 使用正则表达式将除字母数字之外的值排除
+    String actual = s.replaceAll("[^A-Za-z0-9]", "").toLowerCase();
+    // 将得到的结果反转
+    String reverseStr = new StringBuffer(actual).reverse().toString();
+    // 最终返回两字符串比较的值
+    return actual.equals(reverseStr);
+  }
+
+  /**
+   * 双指针解法
+   *
+   */
+  public static boolean isPalindrome3(String s) {
+    if (s == null || "".equals(s) || s.length() == 0) {
+      return true;
+    }
+    int length = s.length();
+    // 使用正则表达式将除字母数字之外的值排除
+    int left = 0, right = length - 1;
+    while (left < right) {
+      // 当当前下标字符不为字母或数字时，跳过该项
+      while (left < right && !Character.isLetterOrDigit(s.charAt(left))) {
+        left++;
+      }
+      while (left < right && !Character.isLetterOrDigit(s.charAt(right))) {
+        right --;
+      }
+      if (Character.toLowerCase(s.charAt(left)) != Character.toLowerCase(s.charAt(right))) {
+        return false;
+      }
+      left++;
+      right--;
     }
     return true;
   }
